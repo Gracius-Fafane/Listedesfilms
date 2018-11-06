564c951abc020b1b341c95b9eb0b6a5616b25eb2
Gracius Fafane <fannygracius@gmail.com>
2018-11-05 23:54:33 -0500

    passing title and overview to seconde activity

--- app/src/main/java/com/example/fafoo/listedesfilms/FlicksActivity.java
+++ app/src/main/java/com/example/fafoo/listedesfilms/FlicksActivity.java
@@ -3,6 +3,8 @@ package com.example.fafoo.listedesfilms;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.fafoo.listedesfilms.models.Movie;
 
@@ -10,9 +12,26 @@ import java.lang.invoke.MethodType;
 
 public class FlicksActivity extends AppCompatActivity {
 
+    ImageView ivImageDetail;
+    TextView tvTitle_Detail;
+    TextView tvOverview_Detail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flicks);
+
+        ivImageDetail = findViewById(R.id.ivImageDetail);
+        tvTitle_Detail = findViewById(R.id.tvTitle_Detail);
+        tvOverview_Detail = findViewById(R.id.tvOverview_Detail);
+
+       Movie movie = (Movie) getIntent().getSerializableExtra("movie");
+
+
+      String Title = movie.getTitle();
+      String Overview = movie.getOverview();
+
+      tvTitle_Detail.setText(Title);
+      tvOverview_Detail.setText(Overview);
     }
 }
